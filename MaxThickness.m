4bcc24a53de0fa38f6e79e9056ae000622cea5d3
Ionut Alexandru Cosuleanu <alex24rc@gmail.com>
2017-07-15 17:46:25 +0200

    Update
    Update

---
+++ MaxThickness.m
@@ -0,0 +1,13 @@
+function [ Thick_max ] = MaxThickness( Thickness_Profile )
+%MAXTHICKNESS Summary of this function goes here
+%   Input: Thickness_Profile: First row is the x coord while the second,
+%   the thickness
+%   Output: Maximum Thickness of the Profile and the x-coord
+%% Load profile
+ProfileFileName = strcat(Thickness_Profile,'.txt');
+Thickness = load(ProfileFileName);
+%Multiply by the chord 1
+[T, I] = max(Thickness(2,:)); % 100%
+Thick_max = [Thickness(1,I), T]; % 100%
+end
+
